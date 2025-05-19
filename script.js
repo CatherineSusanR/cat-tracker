2bd68e8d6754816437747a25e179aa7565a11e3a
Catherine Susan Rajesh <catherineanna04@gmail.com>
2025-05-19 12:51:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function markFeeding() {
     const now = new Date().toLocaleTimeString();
-    document.getElementById('feeding-time').innerText = now;
+    document.getElementById('feeding-time').innerText = now + " 😻";
   }
   
   function saveMood() {
     const mood = document.getElementById('mood-select').value;
-    document.getElementById('mood').innerText = mood;
+    document.getElementById('mood').innerText = mood + " ✨";
   }
   
